ace93c5e2e6531b08d9ff2234adad68c7a6918bf
alightsleeper <alightsleeper@gmail.com>
2024-04-25 16:00:03 -0500

    update default test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/A boutique agency serving artists, non-profits, startups and small businesses./i);
   expect(linkElement).toBeInTheDocument();
 });
